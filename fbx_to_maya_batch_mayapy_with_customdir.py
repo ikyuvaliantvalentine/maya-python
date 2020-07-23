2af6814b9b5fc69e7598fff820f753ca47b20ac4
ikyuvaliantvalentine <valiantvalentine5@gmail.com>
2020-07-23 14:04:25 +0700

    Update fbx_to_maya_batch_mayapy_with_customdir.py

--- fbx_to_maya_batch_mayapy_with_customdir.py
+++ fbx_to_maya_batch_mayapy_with_customdir.py
@@ -4,7 +4,7 @@ import os
 import maya.cmds as cmds
 import maya.mel as MEL
 path="D:/Submission/FBX_NEW/"
-path_maya = "D:/Submission/"
+path_maya = "D:/Submission/MAYA_FILE/"
 matches = []
 
 cmds.loadPlugin("fbxmaya", quiet=True)
@@ -18,13 +18,20 @@ for i in matches:
     fnamea = str(i).split('\\')[-1:][0]
     fname = str(i).rsplit("\\",1)
     final = (os.path.basename(fname[0]))
-    fnamea = str(i).split('\\')[-1:][0]
-    MEL.eval('string $fnamea = `python "fnamea"`;')
-    result = os.path.splitext(fnamea)[0]
-    MEL.eval('FBXImport -f $fnamea')
-    cmds.file(rename=path_maya + final +".ma")
-    cmds.file(save=True, type="mayaAscii")
-    cmds.file(new=1, f=1)
+    tanpaextensi = (os.path.splitext(final)[0])
+    PATH = (path_maya + filename)
+    # Import Functions
+    try:
+        if not os.path.isfile(PATH):
+            MEL.eval('string $fnamea = `python "fnamea"`;')
+            MEL.eval('FBXImport -f $fnamea')
+            cmds.file(rename=path_maya + tanpaextensi +".ma")
+            cmds.file(save=True, type="mayaAscii")
+            cmds.file(new=1, f=1)
+        else:
+            pass
+    except:
+        pass
 
 checking = []
 for root, dirnames, filenames in os.walk(path_maya):
@@ -34,20 +41,32 @@ for root, dirnames, filenames in os.walk(path_maya):
 
 for a in checking:
     count=checking.index(a)
-    print 'Doing %s: %s of %s' % (i,count+1,len(checking))
+    print 'Doing %s: %s of %s' % (a,count+1,len(checking))
     fname = str(a).rsplit("\\",1)
     final = (os.path.basename(fname[0]))
     tanpaextensi = (os.path.splitext(final)[0])
     filename = tanpaextensi + '.ma'
     PATH = (path_maya + filename)
-    if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-        print("File exists and is readable")
+    try:
+        if not os.path.isfile(PATH):
+            fnamea = str(a).split('\\')[-1:][0]
+            MEL.eval('string $fnamea = `python "fnamea"`;')
+            result = os.path.splitext(fnamea)[0]
+            MEL.eval('FBXImport -f $fnamea')
+            cmds.file(rename=result+".ma")
+            cmds.file(save=True, type="mayaAscii")
+            cmds.file(new=1, f=1)
+            print ("File " + tanpaextensi +  " doesnt exist, created new file in " + path)
+        else:
+            pass
+    except:
+        pass
+    # Auto Open Directory
+    if len(path_maya) > 0:
+        try:
+            os.startfile(path_maya)
+        except:
+            subprocess.Popen(['xdg-open', act.export_dir])
     else:
-        fnamea = str(a).split('\\')[-1:][0]
-        MEL.eval('string $fnamea = `python "fnamea"`;')
-        result = os.path.splitext(fnamea)[0]
-        MEL.eval('FBXImport -f $fnamea')
-        cmds.file(rename=result+".ma")
-        cmds.file(save=True, type="mayaAscii")
-        cmds.file(new=1, f=1)
-        print ("File " + tanpaextensi +  " doesnt exist, created new file in " + path)
+        pass
+
